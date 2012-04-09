e264ac072882ec16746a9782c1c72d865490e025
Jonathan Abourbih <jon.abourbih@gmail.com>
2012-04-09 16:00:44 +0100

    Add unit test for message processor

---
+++ server/src/test/java/com/semation/jstatsd/server/messaging/disruptor/DisruptorMessageProcessorTest.java
@@ -0,0 +1,32 @@
+package com.semation.jstatsd.server.messaging.disruptor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jabourbih
+ * Date: 09/04/2012
+ * Time: 15:48
+ */
+public class DisruptorMessageProcessorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testPublish() throws Exception {
+
+    }
+
+    @Test
+    public void testStart() throws Exception {
+
+    }
+
+    @Test
+    public void testShutdown() throws Exception {
+
+    }
+}
